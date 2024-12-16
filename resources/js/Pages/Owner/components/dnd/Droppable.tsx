edfa94309306f1582be5026605c1e23741f4cd5c
shun144 <noelgld13@gmail.com>
2024-12-16 22:15:18 +0900

    アンケート作成画面にpanel追加

--- resources/js/Pages/Owner/components/dnd/Droppable.tsx
+++ resources/js/Pages/Owner/components/dnd/Droppable.tsx
@@ -1,20 +1,44 @@
 import { useDroppable } from "@dnd-kit/core";
-import { FC, ReactNode, memo } from "react";
+import { FC, ReactNode, memo, PropsWithChildren } from "react";
 
-type DroppableProp = {
-  id: string;
-  children: ReactNode;
+type Props = {
+    id: string;
 };
 
-const Droppable: FC<DroppableProp> = ({ children, id }) => {
-  const { setNodeRef, isOver } = useDroppable({
-    id
-  });
-  return (
-    <div ref={setNodeRef} className='bg-slate-950 w-[95%]' >
-      {children}
-    </div>
-  );
+const Droppable = ({ children, id }: PropsWithChildren<Props>) => {
+    const { setNodeRef } = useDroppable({
+        id,
+    });
+    return (
+        <div ref={setNodeRef} className="bg-slate-950 w-full">
+            {children}
+        </div>
+    );
 };
 
 export default memo(Droppable);
+
+// import { useDroppable } from "@dnd-kit/core";
+// import { FC, ReactNode, memo, PropsWithChildren } from "react";
+
+// type Props = {
+//     id: string;
+// };
+
+// // type DroppableProp = {
+// //     id: string;
+// //     children: ReactNode;
+// // };
+
+// const Droppable: FC<DroppableProp> = ({ children, id }) => {
+//     const { setNodeRef, isOver } = useDroppable({
+//         id,
+//     });
+//     return (
+//         <div ref={setNodeRef} className="bg-slate-950">
+//             {children}
+//         </div>
+//     );
+// };
+
+// export default memo(Droppable);
